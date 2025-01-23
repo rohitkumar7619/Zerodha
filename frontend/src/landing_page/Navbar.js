964b487a852aee2a18483341e199c7573beb229b
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-23 07:17:32 +0530

    setting home and connecting all page with homepage then homepage import in index.js

---
+++ frontend/src/landing_page/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NaverBar() {
+  return <h1>NavBar</h1>;
+}
+
+export default NaverBar;
---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Pricingpage() {
+  return <h1>Pricingpage</h1>;
+}
+
+export default Pricingpage;

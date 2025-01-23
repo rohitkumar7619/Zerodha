a23e47d4d86ce8a612ab32062c662985b2ba650c
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-23 20:00:56 +0530

    added bootstrap through cdn link and script also and one more library fontawesome

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -1,11 +1,7 @@
-import React from 'react';
-
+import React from "react";
 
 function Team() {
-  return ( <h1>
-    Team
-  </h1> );
+  return <h1>Team</h1>;
 }
 
-export default Tea<h1>
-Team</h1>m;
+export default Team;

5e9805b1a3ef4cbbbf5a2ad2070611562752792a
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-23 07:41:16 +0530

    setting about and connecting all page with aboutpage then aboutpage import to the index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
 import Pricingpage from "./landing_page/pricing/PricingPage";
+import AboutPage from "./landing_page/about/AboutPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,6 +11,7 @@ root.render(
     <div>
       <HomePage />
       <Pricingpage />
+      <AboutPage />
     </div>
   </React.StrictMode>
 );

8812e11e3fb2a4633162e1642ceddc9aeba1de0a
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-24 11:25:25 +0530

    naver bar completed and styling is also done

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,7 +1,62 @@
 import React from "react";
 
-function NaverBar() {
-  return <h1>NavBar</h1>;
-}
+const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg bg-body-tertiary border-bottom sticky-top"
+      style={{ backgroundColor: "#e3f2fd" }}
+    >
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img
+            src="media/images/logo.svg"
+            alt="Logo"
+            style={{ width: "30%" }}
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Signup
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Product
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Support
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default NaverBar;
+export default Navbar;

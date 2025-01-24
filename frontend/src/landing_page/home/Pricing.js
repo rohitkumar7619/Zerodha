7a4ba5a599c4dae837640beaba5ee324ff9d5916
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-24 09:44:12 +0530

    pricing section or pricing.js is done 80% of the home page is completed

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Pricing() {
-  return <h1>pricing</h1>;
+  return (
+    <div className="container mt-5 mb-5">
+      <div className="row">
+        <div className="col-5">
+          <h1>Unbeatable pricing</h1>
+          <p className="text-muted">
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>
+            See pricing{" "}
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>{" "}
+          </a>
+        </div>
+        <div className="col-7"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;

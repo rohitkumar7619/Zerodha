43699bd5534794fd52b5f52494e748cea55a5eae
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-26 21:41:00 +0530

    support page is not working and react is not running properly

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,24 +1,7 @@
 import React from "react";
 
 function Hero() {
-  return (
-    <div className="container p-5">
-      <div className="row  text-center">
-        <img src="media/images/homeHero.png" alt="Hero img" className="mb-5" />
-        <h1 className="mt-5">Invest in everything</h1>
-        <p>
-          Online platform to invest in stocks, derivatives, mutual funds, ETFs,
-          bonds, and more.
-        </p>
-        <button
-          className="p-3 btn btn-primary"
-          style={{ width: "20%", margin: "0 auto" }}
-        >
-          Sign up for free
-        </button>
-      </div>
-    </div>
-  );
+  return <h1>Hero</h1>;
 }
 
 export default Hero;

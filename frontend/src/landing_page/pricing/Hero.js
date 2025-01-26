7fac1339f1cee3fbc376efaf2b0d86a913a8b9f8
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-26 12:42:32 +0530

    working on pricing page and hero section is done

--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -1,7 +1,41 @@
 import React from "react";
 
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <div className="container mt-5">
+      <div className="row  text-center ">
+        <h1>Pricing</h1>
+        <p className="text-muted mb-5">
+          Free equity investments and flat ₹ 20 traday and F&O trades
+        </p>
+        <hr />
+        <div className="col-4">
+          <img src="media/images/pricingEquity.svg " alt="pricingEquity" />
+          <h1 className="fs-3">Pricing</h1>
+          <p>
+            All equity investment (NSE, BSE), are absolutely free - ₹ 0
+            brokerage
+          </p>
+        </div>
+        <div className="col-4">
+          <img src="media/images/intradayTrades.svg" alt="intradayTrades" />
+          <h1 className="fs-3">Intraday and F&O trades</h1>
+          <p className="text-muted">
+            Flat Rs. 20 or 0.03% (whichever is lower) per executed order on
+            intraday trades across equity, currency, and commodity trades.
+          </p>
+        </div>
+        <div className="col-4">
+          <img src="media/images/pricingEquity.svg " alt="pricingEquity" />
+          <h1 className="fs-3">Free direct MF</h1>
+          <p className="text-muted">
+            All direct mutual fund investments are absolutely free — ₹ 0
+            commissions & DP charges.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Brokerage from "./Brokerage";
 import Hero from "./Hero";
+import OpenAccount from "../OpenAccount";
 
 function Pricingpage() {
   return (
     <>
       <Hero />
+      <OpenAccount />
       <Brokerage />
     </>
   );

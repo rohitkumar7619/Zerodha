ca7466472969e64336a2fd04066740c7c5261020
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-27 20:50:02 +0530

    working on menu making it functionable

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { Link } from "react-router-dom";
 const Menu = () => {
+  const [selectedMenu, setSelectedMenu] = useState(0);
+  const [isProfileDropdownOpen, serIsProfileDropdownOpen] = useState(false);
+
+  const handleMenuClick = (index) => {
+    selectedMenu(index);
+  };
+
+  const handleProfileClick = (index) => {
+    isProfileDropdownOpen(!isProfileDropdownOpen);
+  };
+
+  const menuClass = "menu";
+  const activeMenuClass = "menu selected";
+
   return (
     <div className="menu-container">
       <img src="logo.png" style={{ width: "50px" }} />
       <div className="menus">
         <ul>
           <li>
-            <p>Dashboard</p>
+            <link style={{ textDecoration: "nome" }} to="/">
+              <p>Dashboard</p>
+            </link>
           </li>
           <li>
             <p>Orders</p>

0fb4fbbe43490aff50097c66626b5d66b0539eb2
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-02-01 13:52:13 +0530

    now we can buy shares functionality is added to it

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const { HoldingsModel } = require("./model/HoldingModel");
 
 const { PositionsModel } = require("./model/PositionsModel");
 
+const { ordersModel, OrdersModel } = require("./model/OrdersModel");
+
 const PORT = process.env.PORT || 3002;
 const url = process.env.MONGO_URL;
 
@@ -25,6 +27,19 @@ app.get("/allHoldings", async (req, res) => {
 app.get("/allPositions", async (req, res) => {
   let allPositions = await PositionsModel.find({});
   res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("order save");
 });
 
 app.listen(PORT, () => {

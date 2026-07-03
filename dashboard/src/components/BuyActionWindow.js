377b29eccb9e8044e62b93e48f5bb3a7627fb261
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2026-07-03 09:10:20 +0530

    Update BuyActionWindow component in dashboard

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -13,7 +13,7 @@ const BuyActionWindow = ({ uid }) => {
   const generalContext = useContext(GeneralContext);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post(`${process.env.REACT_APP_API_URL}/newOrder`, {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
